cff1ba205e607446a0e39a21ebae4551a16c3cf2
ChauRuon <chauruon.lci@gmail.com>
2024-04-18 13:19:58 +0700

    button Download

--- src/app/component/view/HeroSection.tsx
+++ src/app/component/view/HeroSection.tsx
@@ -42,11 +42,11 @@ export const HeroSection = () => {
             <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>
               Hire Me
             </button>
-            {/* <button className='px-1 py-1 w-full sm:w-fit rounded-full sm:mt-auto mt-3 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white '>
+            <button className='px-1 py-1 w-full sm:w-fit rounded-full sm:mt-auto mt-3 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white '>
               <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
-            </button> */}
+            </button>
 
-            <button type="button" onClick={() => DownloadFile()}>Download CV</button>
+            {/* <button type="button" onClick={() => DownloadFile()}>Download CV</button> */}
           </div>
         </div>
         <div className='col-span-4 place-self-center mt-4 lg:mt-0'>
